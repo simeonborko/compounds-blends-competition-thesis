7f305f41813f40df78979cfdfb0296ce66c648c6
Simeon Borko <simeon.borko@gmail.com>
2018-08-06 15:48:38 +0200

    getitem, setitem

--- Entity.py
+++ Entity.py
@@ -7,15 +7,18 @@ class Entity:
     _PRIMARY = None
 
     def __init__(self, data: dict):
-        self._data = data
+        self.__data = data
         self.__modified = False
 
-    def _update(self, key, value):
+    def __getitem__(self, item):
+        return self.__data[item]
+
+    def __setitem__(self, key, value):
         if key in self._PRIMARY:
             raise Exception('Hodnota v stlpci patriacom do primarneho kluca nemoze byt zmenena')
-        if self._data[key] != value:
-            self._data[key] = value
+        if self.__data[key] != value:
+            self.__data[key] = value
             self.__modified = True
 
     def get_data(self) -> Union[dict, None]:
-        return {k: v for k, v in self._data.items() if k[:2] == 'G_' or k in self._ALLOWED} if self.__modified else None
+        return {k: v for k, v in self.__data.items() if k[:2] == 'G_' or k in self._ALLOWED} if self.__modified else None
--- SourceWord.py
+++ SourceWord.py
@@ -13,31 +13,31 @@ class SourceWord(Entity):
 
     def __init__(self, data: dict):
         super().__init__(data)
-        self.__lang = self._data['survey_language']
+        self.__lang = self['survey_language']
 
     def sw_syllabic(self):
         if self.__lang == 'SK':
-            self._update('G_sw_syllabic', split_phrase(self._data['sw_graphic'], self.__MATCHER))
+            self['G_sw_syllabic'] = split_phrase(self['sw_graphic'], self.__MATCHER)
 
     def sw_graphic_len(self):
         if self.__lang == 'SK':
-            self._update('G_sw_graphic_len', sk.count_letters(self._data['sw_graphic']))
+            self['G_sw_graphic_len'] = sk.count_letters(self['sw_graphic'])
         elif self.__lang == 'EN':
-            self._update('G_sw_graphic_len', en.count_letters(self._data['sw_graphic']))
+            self['G_sw_graphic_len'] = en.count_letters(self['sw_graphic'])
 
     def sw_phonetic_len(self):
         if self.__lang == 'SK':
-            self._update('G_sw_phonetic_len', sk.count_phones(self._data['sw_phonetic']))
+            self['G_sw_phonetic_len'] = sk.count_phones(self['sw_phonetic'])
         elif self.__lang == 'EN':
-            self._update('G_sw_phonetic_len', en.count_phones(self._data['sw_phonetic']))
+            self['G_sw_phonetic_len'] = en.count_phones(self['sw_phonetic'])
 
     def sw_syllabic_len(self):
         self.sw_syllabic()
         if self.__lang == 'SK':
-            self._update('G_sw_syllabic_len', self._data['G_sw_syllabic'].count('-') + 1)
+            self['G_sw_syllabic_len'] = self['G_sw_syllabic'].count('-') + 1
         elif self.__lang == 'EN':
-            self._update('G_sw_syllabic_len', en.count_syllables(self._data['sw_phonetic']))
+            self['G_sw_syllabic_len'] = en.count_syllables(self['sw_phonetic'])
 
     def frequency_in_snc(self):
         if self.__lang == 'SK':
-            self._update('frequency_in_snc', self.CORPUS.get_frequency(self._data['sw_graphic']))
+            self['frequency_in_snc'] = self.CORPUS.get_frequency(self['sw_graphic'])
