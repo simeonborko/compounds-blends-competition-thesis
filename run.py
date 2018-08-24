61cabac381287e63e7c69ce18bafb0104491d01f
Simeon Borko <simeon.borko@gmail.com>
2018-08-24 12:03:15 +0200

    pogridovane framy

--- run.py
+++ run.py
@@ -5,15 +5,19 @@ from tkinter import *
 from openpyxl import Workbook, load_workbook
 from os.path import isfile
 
-from dependency import DependencyManager
+from manager import SyncManager
 from model import ImageTable, LanguageTable, NamingUnitTable, RespondentTable, ResponseTable, SourceWordTable,\
-    SplinterTable, SplinterView
+    SplinterTable, SplinterView, Overview
 from tools import Connection, ListSaver
 import configuration
 
 
 TABLES = (ImageTable, LanguageTable, NamingUnitTable, RespondentTable, ResponseTable, SourceWordTable, SplinterTable,
           SplinterView)
+SYNC_CLSS = (ImageTable, LanguageTable, NamingUnitTable, RespondentTable, ResponseTable, SourceWordTable, SplinterTable)
+GEN_CLSS = (NamingUnitTable, SourceWordTable, SplinterTable)
+INTEG_CLSS = (NamingUnitTable, SourceWordTable, SplinterTable)
+VIEW_CLSS = (SplinterView, Overview)
 
 
 def export():
@@ -196,47 +200,129 @@ def selected_tables():
 if not configuration.TKINTER_TRACEBACK:
     Tk.report_callback_exception = lambda obj, exc, val, tb: print(val, file=sys.stderr)
 
+
+def make_line(parent, row, **kwargs):
+    Frame(parent, highlightbackground="gray", highlightcolor="gray", highlightthickness=1, bd=0, **kwargs).grid(
+        row=row, column=0, pady=10, padx=10, sticky=W + E, columnspan=2
+    )
+
+
 with ListSaver(configuration.CHECKBOX_FILE, default_checkvals) as saver:
     mw = Tk()
     mw.wm_title("Workbook Tlačítka")
 
-    checkvals = list(saver)
+    exportFrame = Frame(mw)
+    exportFrame.grid(row=0, column=0, columnspan=2, sticky=W+E, pady=10, padx=10)
 
-    for i, tbl in enumerate(TABLES):
-        var = IntVar(value=checkvals.pop(0))
-        checkvars.append(var)
-        Checkbutton(mw, text=tbl.name(), variable=var).grid(row=i, column=0, sticky=W, padx=(0, 30), pady=2)
+    syncLeftFrame = Frame(mw)
+    syncLeftFrame.grid(row=1, column=0, pady=10, padx=10)
+    syncRightFrame = Frame(mw)
+    syncRightFrame.grid(row=1, column=1, pady=10, padx=10)
 
-    force_var = IntVar(value=checkvals.pop(0))
-    corpus_var = IntVar(value=checkvals.pop(0))
+    generateLeftFrame = Frame(mw)
+    generateLeftFrame.grid(row=2, column=0, pady=10, padx=10)
+    generateRightFrame = Frame(mw)
+    generateRightFrame.grid(row=2, column=1, pady=10, padx=10)
 
-    Button(
-        mw,
-        text='Exportovať',
-        command=export,
-    ).grid(row=0, column=1, padx=5, pady=5, rowspan=2, sticky=E+W)
+    integrityLeftFrame = Frame(mw)
+    integrityLeftFrame.grid(row=3, column=0, pady=10, padx=10)
+    integrityRightFrame = Frame(mw)
+    integrityRightFrame.grid(row=3, column=1, pady=10, padx=10)
 
-    Button(
-        mw,
-        text='Synchronizovať',
-        command=sync,
-    ).grid(row=2, column=1, padx=5, pady=5, rowspan=2, sticky=E+W)
+    viewFrame = Frame(mw)
+    viewFrame.grid(row=4, column=0, columnspan=2, sticky=W+E, pady=10, padx=10)
 
-    Button(
-        mw,
-        text='Vyplniť automatizovane',
-        command=generate
-    ).grid(row=4, column=1, padx=5, pady=5, rowspan=2, sticky=E+W)
-    Checkbutton(mw, text='Aj už vyplnené', variable=force_var).grid(row=4, column=2)
-    Checkbutton(mw, text='Korpus', variable=corpus_var).grid(row=5, column=2)
+    # export
+    Button(exportFrame, text='Exportovať').pack(padx=10, pady=10)
 
-    Button(
-        mw,
-        text='Kontrola súdržnosti',
-        command=integrity
-    ).grid(row=6, column=1, padx=5, pady=5, rowspan=2, sticky=E+W)
+    # sync
+    for i, cls in enumerate(SYNC_CLSS):
+        Checkbutton(syncLeftFrame, text=cls.name()).grid(row=i, sticky=W, padx=(0, 30), pady=2)
+    Button(syncRightFrame, text='Synchronizovať').pack(padx=10, pady=10)
 
+    # generate
+    for i, cls in enumerate(GEN_CLSS):
+        Checkbutton(generateLeftFrame, text=cls.name()).grid(row=i, sticky=W, padx=(0, 30), pady=2)
+    Button(generateRightFrame, text='Vyplniť automatizovane').grid(padx=10, pady=10, row=0, column=0, rowspan=2)
+    Checkbutton(generateRightFrame, text='Aj už vyplnené').grid(row=0, column=1, sticky=W)
+    Checkbutton(generateRightFrame, text='Korpus').grid(row=1, column=1, sticky=W)
+
+    # integrity
+    for i, cls in enumerate(INTEG_CLSS):
+        Checkbutton(integrityLeftFrame, text=cls.name()).grid(row=i, sticky=W, padx=(0, 30), pady=2)
+    Button(integrityRightFrame, text='Kontrola súdržnosti').pack(padx=10, pady=10)
+
+    # views
+    for i, cls in enumerate(VIEW_CLSS):
+        Button(viewFrame, text=cls.name().upper()).pack(expand=True, side=LEFT, padx=10, pady=10)
+
+
+
+    # Button(
+    #     mw,
+    #     text='Exportovať',
+    # ).grid(row=0, column=0, columnspan=2)
+    #
+    #
+    # exportFrame = Frame(mw, ).pack()
+    # xxx = Frame(mw, height=100, width=100, highlightbackground="gray", highlightcolor="gray", highlightthickness=1, bd=0).pack()
+    # xxx = Frame(mw, height=100, width=100, highlightbackground="gray", highlightcolor="gray", highlightthickness=1, bd=0).pack()
+
+
+    #
+    # for i in range(7):
+    #     Checkbutton(mw, text=i).grid(row=i+1, column=0)
+    # Button(
+    #     mw,
+    #     text='Synchronizovať'
+    # ).grid(row=1, column=1, rowspan=7)
+    #
+    # for i in range(3):
+    #     Checkbutton(mw, text=i).grid(row=i+8, column=0)
+    # Button(
+    #     mw,
+    #     text='Vyplniť automatizovane',
+    # ).grid(row=1, column=8, rowspan=3)
+    # for i in range(2):
+    #     Checkbutton(mw, text=i).grid(row=)
+
+    # checkvals = list(saver)
+    #
+    # for i, tbl in enumerate(TABLES):
+    #     var = IntVar(value=checkvals.pop(0))
+    #     checkvars.append(var)
+    #     Checkbutton(mw, text=tbl.name(), variable=var).grid(row=i, column=0, sticky=W, padx=(0, 30), pady=2)
+    #
+    # force_var = IntVar(value=checkvals.pop(0))
+    # corpus_var = IntVar(value=checkvals.pop(0))
+    #
+    # Button(
+    #     mw,
+    #     text='Exportovať',
+    #     command=export,
+    # ).grid(row=0, column=1, padx=5, pady=5, rowspan=2, sticky=E+W)
+    #
+    # Button(
+    #     mw,
+    #     text='Synchronizovať',
+    #     command=sync,
+    # ).grid(row=2, column=1, padx=5, pady=5, rowspan=2, sticky=E+W)
+    #
+    # Button(
+    #     mw,
+    #     text='Vyplniť automatizovane',
+    #     command=generate
+    # ).grid(row=4, column=1, padx=5, pady=5, rowspan=2, sticky=E+W)
+    # Checkbutton(mw, text='Aj už vyplnené', variable=force_var).grid(row=4, column=2)
+    # Checkbutton(mw, text='Korpus', variable=corpus_var).grid(row=5, column=2)
+    #
+    # Button(
+    #     mw,
+    #     text='Kontrola súdržnosti',
+    #     command=integrity
+    # ).grid(row=6, column=1, padx=5, pady=5, rowspan=2, sticky=E+W)
+    #
     mw.mainloop()
-
-    for i, var in enumerate(checkvars + [force_var, corpus_var]):
-        saver[i] = var.get()
+    #
+    # for i, var in enumerate(checkvars + [force_var, corpus_var]):
+    #     saver[i] = var.get()
