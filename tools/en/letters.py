42caae93c6eb00c0b52141611441e58aa150398f
Simeon Borko <simeon.borko@gmail.com>
2018-08-25 14:55:52 +0200

    anglicky get letters list

--- tools/en/letters.py
+++ tools/en/letters.py
@@ -1,2 +1,17 @@
-def count_letters(expr):
-    return sum([len(x) for x in expr.replace('-','').split(' ')])
+REDUNDANT = ' -'
+
+
+def remove_redundant(word):
+    """Odstrani symboly, ktore robia problemy pri pocitani"""
+    word = word.strip()
+    for r in REDUNDANT:
+        word = word.replace(r, '')
+    return word
+
+
+def count_letters(expr: str) -> int:
+    return len(get_letters_list(expr))
+
+
+def get_letters_list(expr: str) -> list:
+    return list(remove_redundant(expr))
