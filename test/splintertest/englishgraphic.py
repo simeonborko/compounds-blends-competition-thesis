f1b2d0d01ba18fb6101304c09078825629472490
Simeon Borko <simeon.borko@gmail.com>
2018-08-27 16:53:00 +0200

    fix test english graphic

--- test/splintertest/englishgraphic.py
+++ test/splintertest/englishgraphic.py
@@ -8,7 +8,7 @@ class FindSplinter(TestCase):
     def test_0(self):
         s = EnglishGraphicSplinter('Bug', 'bunny', True)
         self.assertTrue(s.find_splinter())
-        self.assertEqual('Bu', s.splinter)
+        self.assertEqual('bu', s.splinter)
         self.assertEqual(2, s.length)
 
     def test_1(self):
@@ -18,7 +18,7 @@ class FindSplinter(TestCase):
         self.assertEqual(2, s.length)
 
     def test_2(self):
-        s = EnglishGraphicSplinter('bock', 'bokk', True)
+        s = EnglishGraphicSplinter('bock', 'bokk', False)
         self.assertTrue(s.find_splinter())
         self.assertEqual('bock', s.splinter)
         self.assertEqual(4, s.length)
@@ -28,8 +28,8 @@ class SetSplinter(TestCase):
 
     def test_0(self):
         s = EnglishGraphicSplinter('Bug', 'bunny', True)
-        self.assertTrue(s.set_splinter('Bu'))
-        self.assertEqual('Bu', s.splinter)
+        self.assertTrue(s.set_splinter('bu'))
+        self.assertEqual('bu', s.splinter)
         self.assertEqual(2, s.length)
 
     def test_1(self):
@@ -45,13 +45,7 @@ class SetSplinter(TestCase):
         self.assertEqual(1, s.length)
 
     def test_3(self):
-        s = EnglishGraphicSplinter('bock', 'bokk', True)
-        self.assertTrue(s.set_splinter('bock'))
-        self.assertEqual('bock', s.splinter)
-        self.assertEqual(4, s.length)
-
-    def test_4(self):
-        s = EnglishGraphicSplinter('bock', 'bokk', True)
+        s = EnglishGraphicSplinter('bock', 'bokk', False)
         self.assertTrue(s.set_splinter('bock'))
         self.assertEqual('bock', s.splinter)
         self.assertEqual(4, s.length)
