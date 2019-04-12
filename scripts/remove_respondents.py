de7951715a1597024d295c1936745431e01d1a9e
Simeon Borko <simeon.borko@gmail.com>
2019-04-12 22:32:43 +0200

    remove respondents script

---
+++ scripts/remove_respondents.py
@@ -0,0 +1,31 @@
+"""
+Odstrani respondentov a ich odpovede.
+Podmienky: first_language not in [SK, EN] or age < 16
+"""
+
+from tools import Connection
+
+if __name__ == '__main__':
+    with Connection() as conn:
+
+        c = conn.cursor()
+        c.execute(
+            "SELECT respondent_id FROM respondent WHERE first_language != 'SK' AND first_language != 'EN' OR age < 16"
+        )
+        res_ids = [res_id for res_id, in c]
+        for res_id in res_ids:
+
+            conn.cursor().execute(
+                "DELETE FROM response_original WHERE respondent_id = %s",
+                (res_id, )
+            )
+            conn.cursor().execute(
+                "DELETE FROM response_modified WHERE respondent_id = %s",
+                (res_id, )
+            )
+            conn.cursor().execute(
+                "DELETE FROM respondent WHERE respondent_id = %s",
+                (res_id, )
+            )
+
+        conn.commit()
