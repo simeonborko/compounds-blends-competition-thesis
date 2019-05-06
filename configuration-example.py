16658c46749807363790c65b830b4ce1e97b9b0a
Simeon Borko <simeon.borko@gmail.com>
2019-05-06 16:42:50 +0200

    odstranenie starych korpus suborov z konfiguracie

--- configuration-example.py
+++ configuration-example.py
@@ -13,9 +13,6 @@ SYNC_OVERVIEW = False
 
 XLSX_FILE = '/tmp/test.xlsx'
 CHECKBOX_FILE = '/tmp/test.json'
-CORPUS_FILE = '/home/simeon/PycharmProjects/lymis/corpus.json'
-BNC_CORPUS_FILE = '/home/simeon/PycharmProjects/lydkadb/bnc_corpus.json'
-BNC_CORPUS_NOT_FOUND_LIST_FILE = '/home/simeon/PycharmProjects/lydkadb/bnc_corpus_not_found_list.json'
 TKINTER_TRACEBACK = True
 BACKUP_FILE_FMT = '/tmp/test_{}.xlsx.bak'
 CAMBRIDGE_FILE = '/home/simeon/PycharmProjects/lydkadb/cambridge.json'
