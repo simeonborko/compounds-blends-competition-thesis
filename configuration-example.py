42e4203ab8e30e23eca43d0e0c9015a458acfec3
Simeon Borko <simeon.borko@gmail.com>
2020-02-17 21:28:58 +0100

    configuration-example: preusporiadanie podla lydky

--- configuration-example.py
+++ configuration-example.py
@@ -15,14 +15,16 @@ CAMBRIDGE_USER = CAMBRIDGE_DB = 'cambridge'
 CAMBRIDGE_PASSWD = PASSWD
 
 SYNC_OVERVIEW = False
+DEBUG = False
+TKINTER_TRACEBACK = True
 
 XLSX_FILE = '/tmp/test.xlsx'
+
 CHECKBOX_FILE = '/tmp/test.json'
-TKINTER_TRACEBACK = True
-BACKUP_FILE_FMT = '/tmp/test_{}.xlsx.bak'
+
 CAMBRIDGE_BACKUP_DIR = '/home/simeon/PycharmProjects/lydkadb/data/cambridge'
 CORPORA_BACKUP_DIR = '/home/simeon/PycharmProjects/lydkadb/data/corpora'
 CORPORA_SLOVAK_SLEEP_TIME_ERROR = 20
 CORPORA_SLOVAK_SLEEP_TIME_PREVENTIVE = 5
 
-DEBUG = False
+BACKUP_FILE_FMT = '/tmp/test_{}.xlsx.bak'
