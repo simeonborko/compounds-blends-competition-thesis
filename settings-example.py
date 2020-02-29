c7fcbc079fe981644d34f8600d1b09e7b431649c
Simeon Borko <simeon.borko@gmail.com>
2020-02-29 11:06:00 +0100

    settings

---
+++ settings-example.py
@@ -0,0 +1,14 @@
+DB = 'prod_filtered2'
+DB_PROXY = False
+
+SYNC_OVERVIEW = False
+DEBUG = False
+TKINTER_TRACEBACK = False
+
+XLSX_FILE = '/tmp/test.xlsx'
+BACKUP_FILE_FMT = '/tmp/test_{}.xlsx.bak'
+
+CHECKBOX_FILE = '/tmp/test.json'
+
+CAMBRIDGE_BACKUP_DIR = '/home/simeon/PycharmProjects/lydkadb/data/cambridge'
+CORPORA_BACKUP_DIR = '/home/simeon/PycharmProjects/lydkadb/data/corpora'
